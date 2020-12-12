63215950322cfa24ce190cb31f9e401a548e75c9
onecareer-yamamoto <takashi.yamamoto@onecareer.jp>
2020-12-13 00:32:21 +0900

    修正

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-# !/bin/bash
+# !/bin/sh
 XH=~/.xxh \
  && XD=https://github.com/xxh/xxh-portable/raw/master/result/xxh-portable-musl-alpine-Linux-x86_64.tar.gz \
  && mkdir -p $XH && cd $XH \
  && ( [[ -x $(command -v curl) ]] && curl -L $XD || wget -O- $XD ) | tar zxf - xxh \
  && cd ..
-./.xxh/xxh local +s fish  +i +q
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+./.xxh/xxh local +s fish  +i +q
 
