197a6ea42ce8020e6ff32daf49c70323dfc3ce24
onecareer-yamamoto <takashi.yamamoto@onecareer.jp>
2020-12-12 21:05:10 +0900

    dotfiles

---
+++ install.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+XH=~/.xxh \
+ && XD=https://github.com/xxh/xxh-portable/raw/master/result/xxh-portable-musl-alpine-Linux-x86_64.tar.gz \
+ && mkdir -p $XH && cd $XH \
+ && ( [[ -x $(command -v curl) ]] && curl -L $XD || wget -O- $XD ) | tar zxf - xxh \
+ && echo 'Usage: ./xxh local [+s xonsh/zsh/fish/osquery/bash]'
