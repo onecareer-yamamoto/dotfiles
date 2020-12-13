a29976d6f1ed0a79fa29aff63502f1d80479f782
onecareer-yamamoto <takashi.yamamoto@onecareer.jp>
2020-12-13 23:01:42 +0900

    iiiiiiiiiiiiii

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 curl -L git.io/antigen > antigen.zsh
 ln -sf ~/dotfiles/.profile ~/.profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.antigen.zsh ~/.antigen.zsh
+ln -sf ~/dotfiles/antigen.zsh ~/antigen.zsh
 ln -sf ~/dotfiles/.antigenrc ~/.antigenrc
 
