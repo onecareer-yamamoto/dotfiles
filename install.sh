8a8d457a303354ebb7e6f73dc13c997dc85dd068
Takashi Yamamoto <54784518+onecareer-yamamoto@users.noreply.github.com>
2020-12-17 17:32:33 +0900

    .profile削除

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 # !/bin/sh
 curl -L git.io/antigen > antigen.zsh
-ln -sf ~/dotfiles/.profile ~/.profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/antigen.zsh ~/antigen.zsh
 ln -sf ~/dotfiles/.antigenrc ~/.antigenrc
